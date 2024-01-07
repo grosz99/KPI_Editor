f734ac81f7ff5254b2eaeff4649ce2c0ab8a77c4
grosz99 <93449265+grosz99@users.noreply.github.com>
2024-01-07 16:39:26 -0500

    Update kpi_editor.py

--- kpi_editor.py
+++ kpi_editor.py
@@ -6,18 +6,19 @@ import numpy as np
 
 # Load the dataset
 data_url = "https://raw.githubusercontent.com/grosz99/KPI_Editor/main/Sample_Superstore_Streamlit_Github.csv?token=GHSAT0AAAAAACMGXKXOJBA4EXP4MH4M3534ZM3DC5A"
-data = pd.read_csv(data_url)
-
-# Convert 'Order Date' to datetime
-data['Order Date'] = pd.to_datetime(data['Order Date'])
+df = pd.read_csv(data_url)
+# Convert 'Order Date' to datetime format for consistent comparison
+df['Order Date'] = pd.to_datetime(df['Order Date'])
 
 # Step 1: Filters
 st.sidebar.header("Filters")
-start_date = st.sidebar.date_input("Start Ship Date", data['Order Date'].min())
-end_date = st.sidebar.date_input("End Ship Date", data['Order Date'].max())
+start_date = st.sidebar.date_input("Start Ship Date", df['Order Date'].min().date())
+end_date = st.sidebar.date_input("End Ship Date", df['Order Date'].max().date())
+# Convert the Streamlit dates to datetime64[ns] format
 start_date_np = np.datetime64(start_date)
 end_date_np = np.datetime64(end_date)
-filtered_df = data[(data['Order Date'] >= start_date) & (data['Order Date'] <= end_date)]
+
+filtered_df = df[(df['Order Date'] >= start_date_np) & (df['Order Date'] <= end_date_np)]
 
 # Step 2: Data Validation & Editing
 st.header("Validate values and you can change them if they are not right")
