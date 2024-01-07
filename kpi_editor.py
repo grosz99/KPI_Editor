98c19ac76d86330c87b81dbd13140bb1e8a31d24
grosz99 <93449265+grosz99@users.noreply.github.com>
2024-01-07 16:41:52 -0500

    Update kpi_editor.py

--- kpi_editor.py
+++ kpi_editor.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 # Load the dataset
-data_url = "https://raw.githubusercontent.com/grosz99/KPI_Editor/main/Sample_Superstore_Streamlit_Github.csv?token=GHSAT0AAAAAACMGXKXOJBA4EXP4MH4M3534ZM3DC5A"
+data_url = "https://raw.githubusercontent.com/grosz99/KPI_Editor/main/Sample_Superstore_Streamlit_Github.csv?token=GHSAT0AAAAAACMGXKXO3WEEIFUXUQZEMC5CZM3DKZQ"
 df = pd.read_csv(data_url)
 # Convert 'Order Date' to datetime format for consistent comparison
 df['Order Date'] = pd.to_datetime(df['Order Date'])
